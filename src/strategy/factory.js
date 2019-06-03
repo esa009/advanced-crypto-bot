a5285d4ac0e024f5db7ab918acc40dd26ac5a1d0
Almendra <estrada.almendra@gmail.com>
2019-06-03 14:32:11 -0400

    double exp ma

--- src/strategy/factory.js
+++ src/strategy/factory.js
@@ -1,13 +1,16 @@
-const Simple = require('./simple')
-const MACD = require('./simpleMACD')
+const Simple = require('./simple');
+const MACD = require('./simpleMACD');
+const Dema = require('./dema');
 
 exports.create = function(type, data) {
   switch (type) {
     case 'macd':
-      return new MACD(data)
+      return new MACD(data);
     case 'simple':
-      return new Simple(data)
+      return new Simple(data);
+    case 'dema':
+      return new Dema(data);
     default:
-      return new MACD(data)
+      return new MACD(data);
   }
-}
+};
--- src/strategy/index.js
+++ src/strategy/index.js
@@ -1,7 +1,9 @@
-const Simple = require('./simple')
-const SimpleMACD = require('./simpleMACD')
-const Factory = require('./factory')
+const Simple = require('./simple');
+const SimpleMACD = require('./simpleMACD');
+const Factory = require('./factory');
+const Dema = require('./dema');
 
-exports.Simple = Simple
-exports.SimpleMACD = SimpleMACD
-exports.Factory = Factory
+exports.Simple = Simple;
+exports.SimpleMACD = SimpleMACD;
+exports.Factory = Factory;
+exports.Dema = Dema;
